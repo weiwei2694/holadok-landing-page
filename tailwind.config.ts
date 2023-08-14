71bb24ffdf4f1830f2eed64b90c683cd84dc06ae
Arya Priema <99aryaprima@gmail.com>
2023-08-14 22:42:35 +0700

    setting up, and config: assets, colors. font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    colors: {
+      "dark-1": "#000",
+      "blue-1": "#203271",
+      "blue-2": "#101C45",
+      "light-1": "#fff",
+    },
+  },
   plugins: [],
-}
-export default config
+};
+export default config;
